49c92de2f1e69d839339ba2f4cd66ab52ed4ffb2
vlad2882 <43888259+vlad2882@users.noreply.github.com>
2025-09-30 21:23:57 -1000

    test commit to make sure webpage updates

--- wow-stats/src/App.js
+++ wow-stats/src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Test React App for WoW Stats
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
