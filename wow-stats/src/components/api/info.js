8c490f3ff40a304e24684081b5c1159a1353d955
vlad2882 <43888259+vlad2882@users.noreply.github.com>
2025-10-04 11:22:17 -1000

    sample json for player

---
+++ wow-stats/src/components/api/info.js
@@ -0,0 +1,131 @@
+{
+  "character": {
+    "key": {
+      "href": "https://us.api.blizzard.com/profile/wow/character/raden/sora?namespace=profile-classic-us"
+    },
+    "name": "Sora",
+    "id": 99034241,
+    "realm": {
+      "key": {
+        "href": "https://us.api.blizzard.com/data/wow/realm/6361?namespace=dynamic-classic-us"
+      },
+      "id": 6361,
+      "slug": "raden"
+    },
+    "level": 90,
+    "playable_class": {
+      "key": {
+        "href": "https://us.api.blizzard.com/data/wow/playable-class/2?namespace=static-5.5.1_63086-classic-us"
+      },
+      "id": 2
+    },
+    "playable_race": {
+      "key": {
+        "href": "https://us.api.blizzard.com/data/wow/playable-race/1?namespace=static-5.5.1_63086-classic-us"
+      },
+      "id": 1
+    },
+    "faction": {
+      "type": "ALLIANCE"
+    }
+  },
+  "rank": 5,
+  "player": {
+    "_links": {
+      "self": {
+        "href": "https://us.api.blizzard.com/profile/wow/character/raden/sora?namespace=profile-classic-us"
+      }
+    },
+    "id": 99034241,
+    "name": "Sora",
+    "gender": {
+      "type": "MALE",
+      "name": "Male"
+    },
+    "faction": {
+      "type": "ALLIANCE",
+      "name": "Alliance"
+    },
+    "race": {
+      "key": {
+        "href": "https://us.api.blizzard.com/data/wow/playable-race/1?namespace=static-5.5.1_63086-classic-us"
+      },
+      "name": "Human",
+      "id": 1
+    },
+    "character_class": {
+      "key": {
+        "href": "https://us.api.blizzard.com/data/wow/playable-class/2?namespace=static-5.5.1_63086-classic-us"
+      },
+      "name": "Paladin",
+      "id": 2
+    },
+    "active_spec": {
+      "key": {
+        "href": "https://us.api.blizzard.com/data/wow/playable-specialization/70?namespace=static-5.5.1_63086-classic-us"
+      },
+      "name": "Retribution",
+      "id": 70
+    },
+    "realm": {
+      "key": {
+        "href": "https://us.api.blizzard.com/data/wow/realm/6361?namespace=dynamic-classic-us"
+      },
+      "name": "Ra-den",
+      "id": 6361,
+      "slug": "raden"
+    },
+    "guild": {
+      "key": {
+        "href": "https://us.api.blizzard.com/data/wow/guild/raden/mediocre-at-best?namespace=profile-classic-us"
+      },
+      "name": "Mediocre at Best",
+      "id": 22286725,
+      "realm": {
+        "key": {
+          "href": "https://us.api.blizzard.com/data/wow/realm/6361?namespace=dynamic-classic-us"
+        },
+        "name": "Ra-den",
+        "id": 6361,
+        "slug": "raden"
+      },
+      "faction": {
+        "type": "ALLIANCE",
+        "name": "Alliance"
+      }
+    },
+    "level": 90,
+    "experience": 0,
+    "achievement_points": 8050,
+    "achievements": {
+      "href": "https://us.api.blizzard.com/profile/wow/character/raden/sora/achievements?namespace=profile-classic-us"
+    },
+    "titles": {
+      "href": "https://us.api.blizzard.com/profile/wow/character/raden/sora/titles?namespace=profile-classic-us"
+    },
+    "pvp_summary": {
+      "href": "https://us.api.blizzard.com/profile/wow/character/raden/sora/pvp-summary?namespace=profile-classic-us"
+    },
+    "media": {
+      "href": "https://us.api.blizzard.com/profile/wow/character/raden/sora/character-media?namespace=profile-classic-us"
+    },
+    "last_login_timestamp": 1759274951000,
+    "average_item_level": 500,
+    "equipped_item_level": 500,
+    "specializations": {
+      "href": "https://us.api.blizzard.com/profile/wow/character/raden/sora/specializations?namespace=profile-classic-us"
+    },
+    "statistics": {
+      "href": "https://us.api.blizzard.com/profile/wow/character/raden/sora/statistics?namespace=profile-classic-us"
+    },
+    "equipment": {
+      "href": "https://us.api.blizzard.com/profile/wow/character/raden/sora/equipment?namespace=profile-classic-us"
+    },
+    "appearance": {
+      "href": "https://us.api.blizzard.com/profile/wow/character/raden/sora/appearance?namespace=profile-classic-us"
+    },
+    "active_title": {
+      "name": "%s the Fearless"
+    }
+  }
+}
