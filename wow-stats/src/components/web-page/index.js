4f4f7f8dae5d4358d6c3a17d288f77cc50528446
vlad2882 <43888259+vlad2882@users.noreply.github.com>
2025-10-06 12:20:42 -1000

    fixed es-lint warning

--- wow-stats/src/components/web-page/index.js
+++ wow-stats/src/components/web-page/index.js
@@ -239,8 +239,8 @@ const WebPage = () => {
                     <img 
                       alt="" 
                       src={
-                        wowClassInfo[toon?.character?.playable_class?.id]?.icon?.
-                          find(obj => obj[toon?.player?.active_spec?.name])?.[toon?.player?.active_spec?.name] // IF you find the key-val pair}
+                        wowClassInfo[toon?.character?.playable_class?.id]?.icon
+                        ?.find(obj => obj[toon?.player?.active_spec?.name])?.[toon?.player?.active_spec?.name] // IF you find the key-val pair}
                       }
                     />
                   </td>
